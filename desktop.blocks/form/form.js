9560f89648cab82c7c1e31a350fd5c70ee2bcfaa
aristov <vv.aristov@gmail.com>
2014-04-16 23:59:43 +0400

    codestyle

--- desktop.blocks/form/form.js
+++ desktop.blocks/form/form.js
@@ -1,26 +1,26 @@
 modules.define('form', ['i-bem__dom'], function(provide, BEMDOM) {
 
-    provide(BEMDOM.decl('form', {
+provide(BEMDOM.decl('form', {
 
-        onSetMod: {
+    onSetMod: {
 
-            js: {
-                inited: function() {
-                    this.bindTo('submit', this._onSubmit);
-                }
+        js: {
+            inited: function() {
+                this.bindTo('submit', this._onSubmit);
             }
-
-        },
-
-        _onSubmit: function(e) {
-            e.preventDefault();
-            this.emit('submit');
-        },
-
-        getVal: function() {
-            return this.domElem.serialize();
         }
 
-    }, {}));
+    },
+
+    _onSubmit: function(e) {
+        e.preventDefault();
+        this.emit('submit');
+    },
+
+    getVal: function() {
+        return this.domElem.serialize();
+    }
+
+}, {}));
 
 });
