97cb66a18cbe00b01079f5ad1d85316182eaaa7a
aristov <vv.aristov@gmail.com>
2014-04-17 00:23:20 +0400

    add sssr

---
+++ desktop.blocks/sssr/sssr.js
@@ -0,0 +1,31 @@
+modules.define('sssr', ['i-bem__dom', 'jquery'], function(provide, BEMDOM, $) {
+
+provide(BEMDOM.decl('sssr', {
+
+    onSetMod: {
+        js: {
+            inited: function() {
+                this._form = this.findBlockInside('form')
+                                .on('submit', this._sendRequest, this);
+            }
+        }
+    },
+
+    _sendRequest: function() {
+        $.ajax({
+            type: 'GET',
+            dataType: 'html',
+            url: this.params.url,
+            data: this._form.getVal(),
+            cache: false,
+            success: this._onSuccess.bind(this)
+        });
+    },
+
+    _onSuccess: function(result) {
+        BEMDOM.replace(this.findBlockInside('content').domElem, result);
+    }
+
+}));
+
+});
