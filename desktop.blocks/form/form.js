fc1408cbfafeb8ea0562b306b07e1b20a7b04cb2
aristov <vv.aristov@gmail.com>
2014-04-16 23:55:11 +0400

    add form

---
+++ desktop.blocks/form/form.js
@@ -0,0 +1,26 @@
+modules.define('form', ['i-bem__dom'], function(provide, BEMDOM) {
+
+    provide(BEMDOM.decl('form', {
+
+        onSetMod: {
+
+            js: {
+                inited: function() {
+                    this.bindTo('submit', this._onSubmit);
+                }
+            }
+
+        },
+
+        _onSubmit: function(e) {
+            e.preventDefault();
+            this.emit('submit');
+        },
+
+        getVal: function() {
+            return this.domElem.serialize();
+        }
+
+    }, {}));
+
+});
