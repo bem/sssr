f32b3062e20a59adbd4a2b81df420330b08d5bb9
Dima Belitsky <dmitry.belitsky@gmail.com>
2014-04-16 22:51:18 +0400

    Added server-side blocks

---
+++ desktop.blocks/service/_type/service_type_twitter.node.js
@@ -0,0 +1,39 @@
+var Vow = require('vow'),
+    Twitter = require('twit'),
+    config = require('./service_type_twitter.config');
+
+var twit = new Twitter(config);
+
+modules.define('twitter', function(provide) {
+
+    provide({
+        get: function(query) {
+            var dfd = Vow.defer();
+
+            twit.get('search/tweets', { q: query, count: 20 }, function(err, res) {
+
+                if (err) {
+                    console.error(err);
+                    dfd.reject(err);
+                }
+
+                dfd.resolve(res.statuses.map(function(status) {
+                    return {
+                        avatar: status.user.profile_image_url,
+                        userName: status.user.name,
+                        userNick: status.user.screen_name,
+                        postLink: 'https://twitter.com/' + status.user.screen_name,
+                        createdAt:  status.created_at,
+                        text: status.text,
+                        type: 'twitter'
+                    };
+                }));
+            });
+
+            return dfd.promise();
+
+        }
+    });
+
+
+});
