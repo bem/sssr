536b4462dc6741f60d68459f79bb58b6ea1792b7
Dima Belitsky <dmitry.belitsky@gmail.com>
2014-04-16 22:51:18 +0400

    Added server-side blocks

---
+++ desktop.blocks/server/server.node.js
@@ -0,0 +1,79 @@
+modules.require(['instagram', 'twitter'], function(instagram, twitter) {
+
+var fs = require('fs'),
+    PATH = require('path'),
+    VM = require('vm'),
+    HTTP = require('http'),
+    express = require('express'),
+    app = express(),
+    url = require('url'),
+    querystring = require('querystring'),
+    morgan         = require('morgan'),
+    Vow = require('vow'),
+    pathToBundle = PATH.join('.', 'desktop.bundles', 'index');
+
+app
+    .disable('x-powered-by')
+    .use(morgan('dev'))
+    .use(express.static(pathToBundle));
+
+var bemtreeTemplate = fs.readFileSync(PATH.join(pathToBundle, 'index.bemtree.js'), 'utf-8'),
+    BEMHTML = require(PATH.join('../../' + pathToBundle, 'index.bemhtml.js')).BEMHTML;
+
+var context = VM.createContext({
+    console: console,
+    Vow: Vow
+});
+
+VM.runInContext(bemtreeTemplate, context);
+BEMTREE = context.BEMTREE;
+
+app.get('/search', function(req, res) {
+
+    var dataEntries = [];
+
+    searchObj = url.parse(req.url, true).query;
+    var queryString = querystring.escape(searchObj.query),
+        servicesEnabled = [];
+
+    searchObj.twitter && servicesEnabled.push(twitter.get(queryString));
+    searchObj.instagram && servicesEnabled.push(instagram.get(queryString));
+
+    Vow.all(servicesEnabled)
+        .then(function(results) {
+
+            Object.keys(results).map(function(idx) {
+                dataEntries = dataEntries.concat(results[idx]);
+            });
+
+            dataEntries.sort(function(a, b) {
+                return Date(a.createdAt) - Date(b.createdAt);
+            });
+
+            BEMTREE.apply(dataEntries.map(function(dataEntry) {
+                return {
+                    block: 'island',
+                    data: dataEntry,
+                    mods: { type: dataEntry.type }
+                };
+            }))
+                .then(function(bemjson) {
+                    if (searchObj.json) {
+                        return res.end(JSON.stringify(bemjson, '\n', 4));
+                    }
+
+                    res.end(BEMHTML.apply(bemjson));
+
+            });
+
+        })
+        .fail(function() {
+           console.error(arguments);
+        });
+    });
+
+    var server = app.listen(3000, function() {
+        console.log('Listening on port %d', server.address().port);
+    });
+
+});
