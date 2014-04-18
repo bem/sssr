9524aa8358ce0ce76eb64805a7c6777b93a63d58
Evgeniy Konstantinov <sipayrt@yandex-team.ru>
2014-04-18 16:08:50 +0400

    add tokens

--- desktop.blocks/service/_type/service_type_twitter.config.js
+++ desktop.blocks/service/_type/service_type_twitter.config.js
@@ -1,6 +1,6 @@
 module.exports = {
-    consumer_key: '',
-    consumer_secret: '',
-    access_token: '',
-    access_token_secret: ''
+    consumer_key: 'UBvIdyLNscToLGMJjehTVjsyn',
+    consumer_secret: 'ExZP2gdZDFKaZGAAP6mTwUb0b4AUUfl1uOHw5A6xmS9g3b48Cr',
+    access_token: '2451167779-pZURihZ05FykhQhn4YyUnlhSHNxsRoJt8OWqSnv',
+    access_token_secret: 'udRBOFvlZhWW8TNBogo03Qy7YMlSbICIgiUtSuXl8Kexr'
 }
