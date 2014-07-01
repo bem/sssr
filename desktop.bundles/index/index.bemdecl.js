09b315d0be5a074850babf9b83f14f7d1e2c5a3f
Bemer <bemer@yandex-team.ru>
2014-07-01 15:14:23 +0400

    Add *.bundles files

---
+++ desktop.bundles/index/index.bemdecl.js
@@ -0,0 +1,192 @@
+exports.blocks = [
+    {
+        "name": "page",
+        "elems": [
+            {
+                "name": "meta"
+            },
+            {
+                "name": "css"
+            },
+            {
+                "name": "js"
+            }
+        ]
+    },
+    {
+        "name": "sssr",
+        "mods": [
+            {
+                "name": "autorefresh",
+                "vals": [
+                    true
+                ]
+            }
+        ]
+    },
+    {
+        "name": "header",
+        "elems": [
+            {
+                "name": "logo"
+            },
+            {
+                "name": "form"
+            }
+        ]
+    },
+    {
+        "name": "logo"
+    },
+    {
+        "name": "icon",
+        "mods": [
+            {
+                "name": "type",
+                "vals": [
+                    "sssr"
+                ]
+            }
+        ]
+    },
+    {
+        "name": "form",
+        "elems": [
+            {
+                "name": "search"
+            },
+            {
+                "name": "filter"
+            }
+        ]
+    },
+    {
+        "name": "input",
+        "mods": [
+            {
+                "name": "theme",
+                "vals": [
+                    "normal"
+                ]
+            },
+            {
+                "name": "size",
+                "vals": [
+                    "m"
+                ]
+            },
+            {
+                "name": "has-clear",
+                "vals": [
+                    true
+                ]
+            }
+        ]
+    },
+    {
+        "name": "button",
+        "mods": [
+            {
+                "name": "theme",
+                "vals": [
+                    "normal"
+                ]
+            },
+            {
+                "name": "size",
+                "vals": [
+                    "m"
+                ]
+            }
+        ]
+    },
+    {
+        "name": "spin",
+        "mods": [
+            {
+                "name": "theme",
+                "vals": [
+                    "normal"
+                ]
+            },
+            {
+                "name": "size",
+                "vals": [
+                    "s"
+                ]
+            }
+        ]
+    },
+    {
+        "name": "checkbox",
+        "mods": [
+            {
+                "name": "theme",
+                "vals": [
+                    "normal"
+                ]
+            },
+            {
+                "name": "size",
+                "vals": [
+                    "l"
+                ]
+            },
+            {
+                "name": "checked",
+                "vals": [
+                    true
+                ]
+            }
+        ]
+    },
+    {
+        "name": "content"
+    },
+    {
+        "name": "island",
+        "elems": [
+            {
+                "name": "header"
+            },
+            {
+                "name": "text"
+            },
+            {
+                "name": "footer"
+            }
+        ]
+    },
+    {
+        "name": "user",
+        "elems": [
+            {
+                "name": "name"
+            },
+            {
+                "name": "post-time"
+            },
+            {
+                "name": "icon"
+            }
+        ]
+    },
+    {
+        "name": "link"
+    },
+    {
+        "name": "image"
+    },
+    {
+        "name": "service",
+        "mods": [
+            {
+                "name": "type",
+                "vals": [
+                    "instagram",
+                    "twitter"
+                ]
+            }
+        ]
+    }
+];
