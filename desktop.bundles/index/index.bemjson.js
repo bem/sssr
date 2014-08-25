14e75b7201af88514d5b6b85ba19033e4623526a
aristov <vv.aristov@gmail.com>
2014-08-25 20:19:05 +0400

    move sssr to page content

--- desktop.bundles/index/index.bemjson.js
+++ desktop.bundles/index/index.bemjson.js
@@ -7,83 +7,81 @@
         { elem: 'css', url: '_index.css' }
     ],
     scripts: [{ elem: 'js', url: '_index.js' }],
-    mix: {
+    content: {
         block: 'sssr',
         mods: { autoscroll: true },
-        js: { url: '/search' }
-    },
-    content: [
-        {
-            block: 'sssr',
-            elem: 'header',
-            content: [
-                {
-                    elem: 'logo',
-                    content: [
-                        {
-                            block: 'icon',
-                            mods: { type: 'sssr' }
-                        },
-                        'Social Services Search Robot:'
-                    ]
-                },
-                {
-                    block: 'form',
-                    content: [
-                        {
-                            elem: 'search',
-                            content: [
-                                {
-                                    block: 'input',
-                                    mods: { theme: 'normal', size: 'm', 'has-clear' : true },
-                                    name: 'query',
-                                    val: 'bemup',
-                                    placeholder: 'try me, baby!'
-                                },
-                                {
-                                    block : 'button',
-                                    mods: { theme: 'normal', size: 'm' },
-                                    type: 'submit',
-                                    text: 'Найти'
-                                },
-                                {
-                                    block : 'button',
-                                    mods: { theme: 'normal', size: 'm', togglable: 'check', checked: true },
-                                    mix: { block: 'sssr', elem: 'autoscroll', js: true },
-                                    text: 'Автоскролл'
-                                },
-                                {
-                                    block: 'spin',
-                                    mods: { theme: 'normal', size : 's' }
-                                }
-                            ]
-                        },
-                        {
-                            elem: 'filter',
-                            content: ['twitter', 'instagram', 'yafotki', 'yablogs'].map(function(service) {
-                                return {
-                                    block: 'checkbox',
-                                    mods: {
-                                        theme: 'normal',
-                                        size: 'l',
-                                        checked: service === 'twitter' || service === 'instagram'
+        js: { url: '/search' },
+        content: [
+            {
+                elem: 'header',
+                content: [
+                    {
+                        elem: 'logo',
+                        content: [
+                            {
+                                block: 'icon',
+                                mods: { type: 'sssr' }
+                            },
+                            'Social Services Search Robot:'
+                        ]
+                    },
+                    {
+                        block: 'form',
+                        content: [
+                            {
+                                elem: 'search',
+                                content: [
+                                    {
+                                        block: 'input',
+                                        mods: { theme: 'normal', size: 'm', 'has-clear' : true },
+                                        name: 'query',
+                                        val: 'bemup',
+                                        placeholder: 'try me, baby!'
                                     },
-                                    name : service,
-                                    text : service
-                                };
-                            })
-                        }
-                    ]
-                }
-            ]
-        },
-        {
-            block: 'sssr',
-            elem: 'content'
-        },
-        {
-            block: 'icon',
-            mods: { type: 'bem' }
-        }
-    ]
+                                    {
+                                        block : 'button',
+                                        mods: { theme: 'normal', size: 'm' },
+                                        type: 'submit',
+                                        text: 'Найти'
+                                    },
+                                    {
+                                        block : 'button',
+                                        mods: { theme: 'normal', size: 'm', togglable: 'check', checked: true },
+                                        mix: { block: 'sssr', elem: 'autoscroll', js: true },
+                                        text: 'Автоскролл'
+                                    },
+                                    {
+                                        block: 'spin',
+                                        mods: { theme: 'normal', size : 's' }
+                                    }
+                                ]
+                            },
+                            {
+                                elem: 'filter',
+                                content: ['twitter', 'instagram', 'yafotki', 'yablogs'].map(function(service) {
+                                    return {
+                                        block: 'checkbox',
+                                        mods: {
+                                            theme: 'normal',
+                                            size: 'l',
+                                            checked: service === 'twitter' || service === 'instagram'
+                                        },
+                                        name : service,
+                                        text : service
+                                    };
+                                })
+                            }
+                        ]
+                    }
+                ]
+            },
+            {
+                elem: 'content'
+            },
+            {
+                block: 'icon',
+                mods: { type: 'bem' }
+            }
+        ]
+    }
 })
