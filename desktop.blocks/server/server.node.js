b83df2090ea25e22be90c9a2aa1f759d0505be5b
Dima Belitsky <dmitry.belitsky@gmail.com>
2014-08-26 16:55:40 +0400

    Additional header added

--- desktop.blocks/server/server.node.js
+++ desktop.blocks/server/server.node.js
@@ -19,9 +19,10 @@ var fs = require('fs'),
 
 app
     .use(function(req, res, next) {
-        res.header("Access-Control-Allow-Origin", "*");
         res.header('Access-Control-Allow-Origin', "http://localhost");
+        res.header("Access-Control-Allow-Origin", "*");
         res.header("Access-Control-Allow-Headers", "X-Requested-With");
+        res.header("Access-Control-Allow-Methods: GET, POST, OPTIONS");
         next();
     })
     .disable('x-powered-by')
