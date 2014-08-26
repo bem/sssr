f52cef1498e92c4a10c9de16031892fd502bc809
Dima Belitsky <dmitry.belitsky@gmail.com>
2014-08-26 16:47:51 +0400

    experimenting with cors

--- desktop.blocks/server/server.node.js
+++ desktop.blocks/server/server.node.js
@@ -20,6 +20,7 @@ var fs = require('fs'),
 app
     .use(function(req, res, next) {
         res.header("Access-Control-Allow-Origin", "*");
+        res.header('Access-Control-Allow-Origin', "http://localhost");
         res.header("Access-Control-Allow-Headers", "X-Requested-With");
         next();
     })
