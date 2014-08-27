6a4e2dcdcd31242e24808fc98dd162cd0c3adcb4
Dima Belitsky <dmitry.belitsky@gmail.com>
2014-08-28 01:05:48 +0400

    added bem ribbon

--- desktop.bundles/index/index.bemjson.js
+++ desktop.bundles/index/index.bemjson.js
@@ -27,6 +27,7 @@
                         'Social Services Search Robot &nbsp;',
                         { 
                             block: 'link',
+                            mods : { theme : 'normal' },
                             content: 'github.com/bem/sssr',
                             url: 'http://github.com/bem/sssr/'
                         }
@@ -82,8 +83,11 @@
                     ]
                 },
                 {
-                    block: 'gh-ribbon',
-                    content: '<a href="https://github.com/bem/sssr/"><img style="position: absolute; top: 0; right: 0; border: 0;" src="https://camo.githubusercontent.com/38ef81f8aca64bb9a64448d0d70f1308ef5341ab/68747470733a2f2f73332e616d617a6f6e6177732e636f6d2f6769746875622f726962626f6e732f666f726b6d655f72696768745f6461726b626c75655f3132313632312e706e67" alt="Fork me on GitHub" data-canonical-src="https://s3.amazonaws.com/github/ribbons/forkme_right_darkblue_121621.png" style="width:50%;height:50%;"></a>'
+                    block: 'link',
+                    content: {
+                        block: 'ribbon'
+                    },
+                    url: 'http://github.com/bem/sssr/',
                 }
             ]
         },
